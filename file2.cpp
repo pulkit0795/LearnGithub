a9249b20067ca749ab265c9d8466f1c099320c41
Siddhant Tiwari <54790933+siddhantiiits@users.noreply.github.com>
2020-10-21 09:32:17 +0530

    Update file2.cpp
    Minor bug fixes 2.0

--- file2.cpp
+++ file2.cpp
@@ -6,21 +6,21 @@ int main(){
 	cout<<"this is a change";
 	cout<<"this is a feature";
 	cout<<"Dont call bugs as features";
+	cout<<"Lol me to bolunga... hahaha";
+	cout<<"this is a change";
+	cout<<"this is a feature";
+	cout<<"this is a change";
+	cout<<"this is a feature";
+	cout<<"this is a change";
+	cout<<"this is a feature";
+	cout<<"this is a change";
+	cout<<"this is a feature";
+	cout<<"this is a change";
+	cout<<"this is a feature";
+	cout<<"this is a change";
+	cout<<"this is a feature";
+	cout<<"this is moo baa";
+	cout<<"this is lalala";
 
-	cout<<"this is a change";
-	cout<<"this is a feature";
-	cout<<"this is a change";
-	cout<<"this is a feature";
-	cout<<"this is a change";
-	cout<<"this is a feature";
-	cout<<"this is a change";
-	cout<<"this is a feature";
-	cout<<"this is a change";
-	cout<<"this is a feature";
-	cout<<"this is a change";
-	cout<<"this is a feature";
-	cout<<"this is a change";
-	cout<<"this is a feature";
-
-	return 0;
+	return 1;
 }
